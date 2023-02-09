6e067f1592aeb3d7306c14953fc5d32f6dfce7e7
Chris Vargas <gqcodes@gmail.com>
2023-02-09 10:38:59 -0500

    add homepage animation

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -62,8 +62,8 @@ const Blog = ({ blog }) => {
       <Head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta http-equiv='content-language' content='en' />
-        <meta content='IE=edge' http-equiv='X-UA-Compatible' />
+        <meta httpEquiv='content-language' content='en' />
+        <meta content='IE=edge' httpEquiv='X-UA-Compatible' />
         <meta content='index, follow' name='robots' />
         <meta
           content='swing trading vs day trading, swing trading, day trading patterns, trading view, best indicators for day trading,indicators, day trading definition, day trading rules, tradingview, day trading for beginners, day trading books, day trading software, day trading websites, day trading strategies, day trading software, day trading stocks, day trading crypto, day trading simulator, day trading canada, day trading reddit, day trading courses, day trading for dummies, day trading robinhood, best day trading platform, is day trading worth it, how to start day trading, robinhood day trading, best day trading stocks, make $100 a day trading cryptocurrency, swing trading vs day trading, is day trading profitable, why do most traders lose money, day trading meme stocks, most day traders lose money, day trading research, is day trading dead, lost all money trading, the daily trader exposed, do professional traders lose money, day trading horror stories reddit, day trading tax horror stories, day trading for dummies review, stories of successful day traders, day trading success stories in india, how to be a successful day trader reddit, day trading after hours, day trading events, crypto coins, crypto trading, crypto currencies, crypto price, crypto login, crypto app, doj crypto, flare crypto, agix crypto, albt crypto, amp crypto price, canto crypto, tectonic crypto price, doj crypto announcement, coti crypto, why is crypto going up, ai crypto coins, apt crypto, rndr crypto, floki crypto, vra crypto, crypto arena capacity, what is a crypto mine, lcx crypto, aptos crypto, gala, gala crypto, bitboy crypto twitter, latest ftm crypto, gala crypto price, crypto news, crypto crash, crypto prices, cryptocurrency, crypto mining, crypto bubbles, crypto.com coin, crypto.com arena, crypto wallet, crypto fear and greed index, luna crypto, polygon crypto, solana crypto, best crypto to buy now, metaverse crypto, amp crypto, best crypto wallet, nft crypto, sandbox crypto, fantom crypto, day trading horrors, day trading horror, day trading horror stories, day trader horror, day trader horrors, day trader horror stories, day traders lose money, day trader loses money, day trading losing money, losing money day trading'
@@ -83,7 +83,7 @@ const Blog = ({ blog }) => {
       </Head>
 
       <div className={style.banner}>
-        <Image src={`https:${featuredImage.fields.file.url}`} width={1200} height={600} alt='feature' />
+        <Image src={`https:${featuredImage.fields.file.url}`} width={1200} height={600} alt='feature' priority />
         <h2>{title}</h2>
       </div>
 
